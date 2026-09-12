68aced56b8f1530fa606fbad14873a3a9ea3ceae
Matheus <matheus.fbtrader@gmail.com>
2026-09-12 17:30:21 -0300

    Remove token do código e usa variável de ambiente

--- bot.py
+++ bot.py
@@ -11,12 +11,13 @@ from telegram.ext import (
     CallbackQueryHandler,
     ContextTypes
 )
+import os 
 
-TOKEN = "8795619658:AAGKXKGv_IND95NiUa5vCbuUV9TB_oDil1o"
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
 LINK_CORRETORA = "https://trade.polariumbroker.com/register?aff=823552&aff_model=revenue&afftrack=telegram"
 
-LINK_CANAL = "https://t.me/+PACHFGmDH_UxZjVh"
+LINK_CANAL = os.getenv("TELEGRAM_CHANNEL_LINK")
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -58,67 +59,66 @@ Você entrou para a tropa que acompanha operações ao vivo e leitura real de me
     )
 
 
+async def liberar_canal(query, context):
+    keyboard = [
+        [InlineKeyboardButton(
+            "🔥 ENTRAR NO CANAL VIP",
+            url="https://t.me/+PACHFGmDH_UxZjVh"
+        )]
+    ]
+
+    await query.message.reply_text(
+        """
+✅ Acesso liberado!
+
+Clique abaixo para entrar no canal VIP:
+""",
+        reply_markup=InlineKeyboardMarkup(keyboard)
+    )
+
 async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     query = update.callback_query
-
     await query.answer()
 
-    # BOTÃO CADASTRO
     if query.data == "criar_conta":
 
         keyboard = [
-
             [InlineKeyboardButton(
                 "🚀 FAZER CADASTRO",
                 url=LINK_CORRETORA
             )],
-
             [InlineKeyboardButton(
-                "🔥 ENTRAR NO CANAL VIP",
-                url=LINK_CANAL
+                "✅ JÁ FIZ O CADASTRO",
+                callback_data="liberar_canal"
             )]
-
         ]
 
         await query.message.reply_text(
             """
 🔥 Perfeito.
 
-Faça sua configuração inicial na corretora abaixo e em seguida entre na sala operacional VIP.
+Faça sua configuração inicial na corretora abaixo.
+
+Depois clique em **JÁ FIZ O CADASTRO** para liberar sua entrada no canal VIP.
 """,
             reply_markup=InlineKeyboardMarkup(keyboard)
         )
 
-    # BOTÃO JÁ CADASTRADO
     elif query.data == "ja_cadastrado":
 
-        keyboard = [
+        await liberar_canal(query, context)
 
-            [InlineKeyboardButton(
-                "🔥 ENTRAR NO CANAL VIP",
-                url=LINK_CANAL
-            )]
+    elif query.data == "liberar_canal":
 
-        ]
-
-        await query.message.reply_text(
-            """
-✅ Acesso liberado.
-
-Clique abaixo para entrar no canal VIP:
-""",
-            reply_markup=InlineKeyboardMarkup(keyboard)
-        )
-
-
+        await liberar_canal(query, context)
 app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
-
 app.add_handler(CallbackQueryHandler(button))
 
 print("BOT ONLINE 🔥")
- 
+
 app.run_polling()
+
 
